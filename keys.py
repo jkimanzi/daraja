23d55fb428e8ce74b7827fe16cbcfb0bbe0cbf39
jkimanzi <36914974+jkimanzi@users.noreply.github.com>
2019-11-04 11:06:32 +0300

    Delete keys.py
    removed keys.py file

--- keys.py
+++
@@ -1,7 +0,0 @@
-business_code = "174379"
-passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-consumer_key = "MBWASErTkup100iYGNGP6IbBoYmSwIvS"
-consumer_secret = "kG5AnYQBIZtpXSEm"
-PhoneNumber = "254722531081"
-shortcode = "601425"
-test_msisdn = "254708374149"
